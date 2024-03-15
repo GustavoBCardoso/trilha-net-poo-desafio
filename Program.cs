6a4796912f556325e941b7e7b9101c2a0495ab0a
Gustavo Borges <gustavob68@gmail.com>
2024-03-15 11:17:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Nokia smartphone1 = new(numero: "112233445", modelo: "Nokia 5600", imei: "N976431K", memoria: 64);
+Iphone smartphone2 = new(numero: "998877665", modelo: "Iphone 12", imei: "I134679E", memoria: 128);
+
+Console.WriteLine("Nokia:");
+smartphone1.Ligar();
+smartphone1.ReceberLigacao();
+smartphone1.InstalarAplicativo(nomeApp: "Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+smartphone1.Ligar();
+smartphone2.ReceberLigacao();
